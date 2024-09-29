19f863de6fd7e2c5a7bf649b2f83cd02c5dfaaae
George <george.alexander.saav@gmail.com>
2024-09-29 18:05:28 +0200

    Some progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import Header from './components/Header'
 import initialEmails from './data/emails'
+import { useState } from 'react'
 
 import './styles/App.css'
 
 function App() {
   // Use initialEmails for state
   console.log(initialEmails)
+
+  const [emails, setEmails] = useState(initialEmails);
+  // const [read, setRead] = useState()
+  
+  // const toggleRead = () => {
+  //   if(read === true) {
+  //     setRead(false)
+  //   }
+  //   else if (read === false) {
+  //     setRead(true)
+  //   }
+  // };
+
+  // const readstatus = ['read', 'unread']
 
   return (
     <div className="app">
@@ -38,9 +53,40 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">{/* Render a list of emails here */}</main>
+      <main className="emails">{emails.map((email) => (
+        <>
+        <li className={`email ${email.read ? 'read' : 'unread'}`} key={email.id}>
+        <div className="select">
+          <input className="select-checkbox"
+          type="checkbox"
+          // onClick={toggleRead(email.read)}
+          />
+        </div>
+        <div className="star">
+          <input className="star-checkbox"
+          type="checkbox"  checked={email.starred === true ? `isChecked` : ``}
+          />
+        </div>
+        <div className="sender">{email.sender}</div>
+        <div className="title">{email.title}</div>
+        </li>
+        </>
+      ))}
+      </main>
     </div>
   )
 }
+
+// function ListEmails () {
+//   return (
+//     <ul>
+//       {
+//         initialEmails.map((email) => {
+//           return <li key={email.id}>{email.title}</li>
+//         })
+//       }
+//     </ul>
+//   )
+// }
 
 export default App
